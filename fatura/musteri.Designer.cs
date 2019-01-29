1e1dec175abba789b3792f8673e2b44e0e43bb43
semvs <semvs@ayvansaray.edu.tr>
2019-01-29 14:08:33 +0300

    son hali

--- fatura/musteri.Designer.cs
+++ fatura/musteri.Designer.cs
@@ -28,92 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtİlAdi = new System.Windows.Forms.TextBox();
+            this.txtMusteriUnvani = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textadress = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxşehir = new System.Windows.Forms.ComboBox();
+            this.comboBoxilce = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.m = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // txtİlAdi
+            // txtMusteriUnvani
             // 
-            this.txtİlAdi.Location = new System.Drawing.Point(97, 29);
-            this.txtİlAdi.Name = "txtİlAdi";
-            this.txtİlAdi.Size = new System.Drawing.Size(288, 20);
-            this.txtİlAdi.TabIndex = 11;
+            this.txtMusteriUnvani.Location = new System.Drawing.Point(97, 29);
+            this.txtMusteriUnvani.Name = "txtMusteriUnvani";
+            this.txtMusteriUnvani.Size = new System.Drawing.Size(288, 20);
+            this.txtMusteriUnvani.TabIndex = 11;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 32);
+            this.label1.Location = new System.Drawing.Point(13, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.Size = new System.Drawing.Size(78, 13);
             this.label1.TabIndex = 10;
-            this.label1.Text = "Şehir";
+            this.label1.Text = "Musteri Unvani";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(324, 113);
+            this.button3.Location = new System.Drawing.Point(327, 151);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 9;
             this.button3.Text = "sil";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(196, 113);
+            this.button2.Location = new System.Drawing.Point(199, 151);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 8;
             this.button2.Text = "güncelle";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(61, 113);
+            this.button1.Location = new System.Drawing.Point(97, 151);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 7;
             this.button1.Text = "ekle";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(61, 159);
+            this.dataGridView1.Location = new System.Drawing.Point(64, 197);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(338, 210);
             this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
-            // textBox1
+            // textadress
             // 
-            this.textBox1.Location = new System.Drawing.Point(97, 58);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(288, 20);
-            this.textBox1.TabIndex = 13;
+            this.textadress.Location = new System.Drawing.Point(97, 58);
+            this.textadress.Multiline = true;
+            this.textadress.Name = "textadress";
+            this.textadress.Size = new System.Drawing.Size(288, 20);
+            this.textadress.TabIndex = 13;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(47, 61);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 12;
-            this.label2.Text = "Şehir";
+            this.label2.Text = "adress";
+            // 
+            // comboBoxşehir
+            // 
+            this.comboBoxşehir.FormattingEnabled = true;
+            this.comboBoxşehir.Location = new System.Drawing.Point(97, 85);
+            this.comboBoxşehir.Name = "comboBoxşehir";
+            this.comboBoxşehir.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxşehir.TabIndex = 14;
+            this.comboBoxşehir.SelectedIndexChanged += new System.EventHandler(this.comboBoxşehir_SelectedIndexChanged);
+            // 
+            // comboBoxilce
+            // 
+            this.comboBoxilce.FormattingEnabled = true;
+            this.comboBoxilce.Location = new System.Drawing.Point(97, 112);
+            this.comboBoxilce.Name = "comboBoxilce";
+            this.comboBoxilce.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxilce.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Müşteri İlçesi";
+            // 
+            // m
+            // 
+            this.m.AutoSize = true;
+            this.m.Location = new System.Drawing.Point(23, 88);
+            this.m.Name = "m";
+            this.m.Size = new System.Drawing.Size(68, 13);
+            this.m.TabIndex = 16;
+            this.m.Text = "Müşteri Şehri";
             // 
             // musteri
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(607, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.m);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxilce);
+            this.Controls.Add(this.comboBoxşehir);
+            this.Controls.Add(this.textadress);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtİlAdi);
+            this.Controls.Add(this.txtMusteriUnvani);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -121,6 +169,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Name = "musteri";
             this.Text = "musteri";
+            this.Load += new System.EventHandler(this.musteri_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -129,13 +178,17 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtİlAdi;
+        private System.Windows.Forms.TextBox txtMusteriUnvani;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textadress;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxşehir;
+        private System.Windows.Forms.ComboBox comboBoxilce;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label m;
     }
 }
--- fatura/musteri.cs
+++ fatura/musteri.cs
@@ -12,9 +12,112 @@ namespace fatura
 {
     public partial class musteri : Form
     {
+        FaturaContext db = new FaturaContext();
+        int secilenID;
+
         public musteri()
         {
             InitializeComponent();
+        }
+
+        private void musteri_Load(object sender, EventArgs e)
+        {
+            ilDoldur();
+            
+            listele();
+        }
+
+        private void listele()
+        {
+            var müşteriler = from m in db.musteriler
+                             select new
+                             {
+                                 m.MusteriId,
+                                 m.MusteriUnvani,
+                                 m.MusteriAdresi,
+                                 m.ilce.IlceAdi,
+                                 sehirAdi=m.ilce.il.IlAdi
+                             };
+            dataGridView1.DataSource = müşteriler.ToList();
+            textadress.Clear();
+            txtMusteriUnvani.Clear();
+            txtMusteriUnvani.Focus();
+
+        }
+
+        private void ilDoldur()
+        {
+            var iller = from il in db.iller select new { il.IlId, il.IlAdi };
+            comboBoxşehir.ValueMember = "IlId";
+            comboBoxşehir.DisplayMember = "IlAdi";
+            comboBoxşehir.DataSource = iller.ToList();
+            
+        }
+
+        private void comboBoxşehir_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ilcedoldur();
+        }
+
+        private void ilcedoldur()
+        {
+            
+            var list = from i in db.ilceler
+                       where    i.IlId == (int)comboBoxşehir.SelectedValue
+                       orderby i.IlceAdi
+                       select new
+                       {
+                           i.IlceId,
+                           i.IlceAdi,
+                           i.IlId
+                       };
+
+            comboBoxilce.DisplayMember = "IlceAdi";
+            comboBoxilce.ValueMember = "IlceId";
+            comboBoxilce.DataSource = list.ToList();
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Musteri m = new Musteri();
+            m.MusteriUnvani = txtMusteriUnvani.Text;
+            m.MusteriAdresi = textadress.Text;
+            m.IlceId = (int)comboBoxilce.SelectedValue;
+            db.musteriler.Add(m);
+            db.SaveChanges();
+            listele();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Musteri m = db.musteriler.Find(secilenID);
+            m.MusteriUnvani = txtMusteriUnvani.Text;
+            m.MusteriAdresi = textadress.Text;
+            m.IlceId = (int)comboBoxilce.SelectedValue;
+            db.SaveChanges();
+            listele();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            secilenID = (int)dataGridView1.CurrentRow.Cells[0].Value;
+            var musteri = db.musteriler.Find(secilenID);
+
+            txtMusteriUnvani.Text = musteri.MusteriUnvani;
+            textadress.Text = musteri.MusteriAdresi;
+           comboBoxilce.SelectedValue= musteri.IlceId;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Musteri m = db.musteriler.Find(secilenID);
+            db.musteriler.Remove(m);
+            db.SaveChanges();
+            listele();
+
         }
     }
 }
